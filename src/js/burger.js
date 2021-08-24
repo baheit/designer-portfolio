340f84833ebc4382b1a2b9a61c1a81d45cf31f77
hihihahoi <32423423@gmail.com>
2021-08-24 23:10:48 +0600

    releas v1

---
+++ src/js/burger.js
@@ -0,0 +1,5 @@
+const burger = document.querySelector('.burger')
+const navbar = document.querySelector('.navbar')
+burger.addEventListener('click', () => {
+    navbar.classList.toggle('active')
+})
